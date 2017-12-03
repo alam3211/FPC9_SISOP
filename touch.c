e62ae77980095f297610cacb4d9706ab5b22b6a7
AlamCahya <alamcahya3211@gmail.com>
2017-12-03 09:21:36 +0700

    touch added

---
+++ touch.c
@@ -0,0 +1,25 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+char buf[512];
+
+int main(int argc, char *argv[]){
+
+  int fd;
+  if(argc <= 1){
+    printf(1, "Argument kurang.gunakan touch *namafile*");
+    exit();
+  }
+  
+  if((fd = open(argv[1], O_RDONLY)) < 0) {
+     printf(1,"File tidak ada, file akan dibuat.\n");  
+     fd = open(argv[1],O_CREATE);
+     close(fd);
+  }
+  else {
+    printf(1,"File sudah ada\n");
+  }
+
+}
