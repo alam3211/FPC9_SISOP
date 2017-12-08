21403d75aa5acfc5f70bf382550768adc7e19bf8
AlamCahya <alamcahya3211@gmail.com>
2017-12-09 02:45:07 +0700

    mv * added

--- mv.c
+++ mv.c
@@ -8,14 +8,35 @@ void move(char *src, char *dest)
 {
  
   if (link(src, dest)!=0) {
-    printf(1,"Unable to move\n");
+    printf(1,"Unable to move, file cant link or not found\n");
     exit();
   }
+  //else printf(1,"Success.\n");
 
   if (unlink(src)!=0) {
     printf(1,"Unable to delete the old one\n");
     exit();
   }
+  //else printf(1,"Delete the old one.\n");
+}
+
+void aster(char *dest){
+	int fd= open(".",O_RDONLY),cek;
+	int lndest= strlen(dest);
+	struct stat st;
+	struct dirent de;
+	char buf[512];
+
+	 while(read(fd, &de, sizeof(de)) == sizeof(de)) {
+        cek= stat(de.name, &st); 
+        if(cek == 0 && st.type == T_FILE) {
+            strcpy(buf, dest);
+            strcpy(&buf[lndest],"/");
+			strcpy(&buf[lndest+1], de.name);
+			//printf(1,"%s\n",buf);
+			move(de.name,buf);
+		}
+	}
 }
 
 int main(int argc, char *argv[]){
@@ -24,20 +45,22 @@ int main(int argc, char *argv[]){
 	char buf[512];
 	struct stat st;
 	
-	if((fd = open(argv[argc-1],O_RDONLY))<0)
-		
+	if((fd = open(argv[argc-1],O_RDONLY))<0){
+	}
+	cek=stat(argv[argc-1],&st);
+	lndest=strlen(argv[argc-1]);
 	
 	if (argc<2){
 		printf(1,"Usage: mv file dest\n");
 	}
 
-	if (argc==2){
-		move(argv[1],argv[2]);
-		exit();
+	if(strcmp(argv[1],"*")==0){
+			//printf(1,"masuk\n");
+			aster(argv[2]);
+			exit();
 	}
+	
 
-	cek=stat(argv[argc-1],&st);
-	lndest=strlen(argv[argc-1]);
 	
 	if(cek==0 && st.type != T_DIR){
 		printf(1,"Dest %s is not a directory.\n",argv[argc-1]);
@@ -49,7 +72,8 @@ int main(int argc, char *argv[]){
 		strcpy(buf,argv[argc-1]);
 		strcpy(&buf[lndest],"/");
 		strcpy(&buf[lndest+1],argv[i]);
-		move(argv[i],buf);        
+		move(argv[i],buf);       
+		printf(1,"ngeeng\n"); 
 	}
 	exit();
 }
